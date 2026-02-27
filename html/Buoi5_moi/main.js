c988fed7ce8e6ed1b742909440483e7d0253b9a0
Nguyen Vu Hieu <hieunv1@d-soft.com.vn>
2026-02-27 14:54:39 +0700

    updatemenu

---
+++ html/Buoi5_moi/main.js
@@ -0,0 +1,7 @@
+let menubar = document.querySelector("#slidemenu");
+let overload = document.querySelector("#overload");
+
+function clickmenu(){
+    menubar.classList.toggle("hienthislide");
+    overload.classList.toggle("hienthioverload");
+}
