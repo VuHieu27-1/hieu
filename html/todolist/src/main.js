30d4e4127fbe28be7621249a84908926b5055122
Nguyen Vu Hieu <hieunv1@d-soft.com.vn>
2026-03-11 17:21:51 +0700

    update code 11/3/2026

--- html/todolist/src/main.js
+++ html/todolist/src/main.js
@@ -1,7 +1,100 @@
-let content_block = document.querySelector('#content_block');
+const content_block = document.querySelector('#content_block');
+const content_block_all = document.querySelector('.content_all');
+const block_end = document.querySelector('#block_fisrt');
+const input_task_name = document.querySelector('#input_task_name');
+let data = localStorage.getItem("info_content_block") ? JSON.parse(localStorage.getItem("info_content_block")) : [];
+let color1 = "linear-gradient(135deg,  rgba(238, 251, 255, 0.8), rgba(99, 247, 239, 0.9) )";
+let color2 = "linear-gradient(180deg,  rgba(157, 243, 186, 0.5), rgba(42, 168, 95, 0.6) )";
+let color3 = "linear-gradient(155deg,  rgba(238, 181, 143, 0.5), rgba(233, 187, 37, 0.6) )";
+let color4 = "linear-gradient(135deg,  rgba(255, 171, 171, 0.7), rgba(201, 26, 99, 0.8) )";
+let color_choose = "linear-gradient(155deg,  rgba(238, 181, 143, 0.5), rgba(233, 187, 37, 0.6) )";
 function add_task() {
     let add_tasks = document.querySelector('#add_tasks');
     let over_load = document.querySelector('#over_load');
     add_tasks.classList.toggle('diplay_block_add_task');
     over_load.classList.toggle('diplay_block_add_task');
 }
+///////////////////////////////XU-LY-JSON//////////////////////////////////////////////////////
+function renderBlocks() {
+    let blocksHtml = `
+        <div class="block_content content_all" id="block_fisrt">
+            <i class="material-icons">apps</i>
+            <p>All Tasks</p>
+            <p class="number_of_task"><i class="material-icons size_point">none</i></p>
+        </div>
+    `;
+    data.forEach((task, index) => {
+        blocksHtml += `
+            <div class="block_content content_all" style="background:${task.color};">
+                <i class="material-icons">assignment</i>
+                <p>${task.name}</p>
+                <p class="number_of_task"><i class="material-icons size_point" onclick="delete_block_task(${index})">delete</i></p>
+            </div>
+        `;
+    });
+    blocksHtml += `
+        <div class="block_content">
+            <i class="material-icons" onclick="add_task()">add</i>
+        </div>
+    `;
+    content_block.innerHTML = blocksHtml;
+}
+
+renderBlocks();
+function select_color_1(){
+    document.querySelector('.color_1').classList.toggle("effect_block");
+    document.querySelector('.color_2').classList.remove("effect_block");
+    document.querySelector('.color_3').classList.remove("effect_block");
+    document.querySelector('.color_4').classList.remove("effect_block");
+    color_choose = color1;
+}
+function select_color_2(){
+    document.querySelector('.color_2').classList.toggle("effect_block");
+    document.querySelector('.color_1').classList.remove("effect_block");
+    document.querySelector('.color_3').classList.remove("effect_block");
+    document.querySelector('.color_4').classList.remove("effect_block");
+    color_choose = color2;
+}
+function select_color_3(){
+    document.querySelector('.color_3').classList.toggle("effect_block");
+    document.querySelector('.color_2').classList.remove("effect_block");
+    document.querySelector('.color_1').classList.remove("effect_block");
+    document.querySelector('.color_4').classList.remove("effect_block");
+    color_choose = color3;
+}
+function select_color_4(){
+    document.querySelector('.color_4').classList.toggle("effect_block");
+    document.querySelector('.color_1').classList.remove("effect_block");
+    document.querySelector('.color_2').classList.remove("effect_block");
+    document.querySelector('.color_3').classList.remove("effect_block");
+    color_choose = color4;
+}
+function add_block_tasks() {
+    const taskName = input_task_name.value;
+    let check_choose_color = false;
+    if(document.querySelector('.color_1').classList.contains("effect_block") || document.querySelector('.color_2').classList.contains("effect_block") 
+        || document.querySelector('.color_3').classList.contains("effect_block") || document.querySelector('.color_4').classList.contains("effect_block") )
+    {
+        check_choose_color = true; 
+    }
+    if (taskName !== "" && check_choose_color) {
+        data.push({
+            name: taskName,
+            color: color_choose 
+        });
+        localStorage.setItem("info_content_block", JSON.stringify(data));
+        renderBlocks();
+        input_task_name.value = "";
+        add_task();
+    }else
+    {
+        alert("Task information is missing.");
+    }
+}
+function delete_block_task(index) {
+    data.splice(index, 1); 
+    localStorage.setItem("info_content_block", JSON.stringify(data));
+    renderBlocks();
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
